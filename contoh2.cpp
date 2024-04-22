a3fd5876444729d7964647f7119099c882e19199
Eginpardewan <127277472+Eginpardewan@users.noreply.github.com>
2024-04-22 17:56:38 +0700

    Add files via upload

---
+++ contoh2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class bangunDatar {
+private:
+    float panjang, lebar;
+public:
+    float luas;
+
+    void input() {
+        cout << "Masukkan Panjangnya = ";
+        cin >> panjang;
+        cout << "Masukkan Lebarnya = ";
+        cin >> lebar;
+    }
+
+    float hitungLuas() {
+        return panjang * lebar;
+    }
+
+    void display() {
+        cout << "Panjangnya = " << panjang << endl;
+        cout << "Lebarnya = " << lebar << endl;
+        cout << "Luasnya = " << hitungLuas() << endl;
+    }
+};
+
+int main() {
+    bangunDatar pp;
+    pp.input();
+    pp.display();
+
+    return 0;
+}
